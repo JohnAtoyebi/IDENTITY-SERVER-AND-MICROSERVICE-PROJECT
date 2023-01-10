c284ed2de8da2986e2d4a5e8de6429ab36faf7ec
John Atoyebi <97730948+JohnAtoyebi@users.noreply.github.com>
2023-01-10 13:48:36 +0100

    identity server and microservice project

---
+++ SecureMicroservices/Movies.API/Data/MoviesContextSeed.cs
@@ -0,0 +1,52 @@
+﻿using Movies.API.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace Movies.API.Data
+{
+    public class MoviesContextSeed
+    {
+        public static void SeedAsync(MoviesAPIContext moviesContext)
+        {
+            if (!moviesContext.Movie.Any())
+            {
+                var movies = new List<Movie>
+                {
+                    new Movie
+                    {
+                        Id = 1,
+                        Genre = "Drama",
+                        Title = "Winning Touch",
+                        Rating = "8.12",
+                        ImageUrl = "images/url",
+                        ReleaseDate = DateTime.Now,
+                        Owner = "Jay"
+                    },
+                    new Movie
+                    {
+                        Id = 2,
+                        Genre = "Love",
+                        Title = "Loving Soul",
+                        Rating = "10.1",
+                        ImageUrl = "images/url",
+                        ReleaseDate = DateTime.Now,
+                        Owner = "Sam"
+                    },
+                    new Movie
+                    {
+                        Id = 3,
+                        Genre = "Games",
+                        Title = "Gaming Soul",
+                        Rating = "40.1",
+                        ImageUrl = "images/url",
+                        ReleaseDate = new DateTime(2022, 3, 4),
+                        Owner = "Kay"
+                    }
+                };
+                moviesContext.Movie.AddRange(movies);
+                moviesContext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ SecureMicroservices/Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,149 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Movies.Client.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {   
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public MovieApiService(IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor)
+        {
+            _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+            _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+        }
+
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var httpClient = _httpClientFactory.CreateClient("MovieAPIClient");
+
+            var request = new HttpRequestMessage(
+                HttpMethod.Get,
+                "/movies");
+
+            var response = await httpClient.SendAsync(
+                request, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
+            return movieList;
+
+            /*var apiClientCredentials = new ClientCredentialsTokenRequest
+            {
+                Address = "https://localhost:5005/connect/token",
+                ClientId = "movieClient",
+                ClientSecret = "secret",
+                Scope = "movieAPI"
+            };
+
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5005");
+            if (disco.IsError)
+            {
+                return null;
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(apiClientCredentials);
+            if (tokenResponse.IsError)
+            {
+                return null;
+            }
+
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:5001/api/movies");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            List<Movie> movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
+            return movieList;
+
+            var movieList = new List<Movie>();
+            movieList.Add
+                (
+                    new Movie
+                    {
+                        Id = 1,
+                        Genre = "Comics",
+                        Title = "asd",
+                        Rating = "9.2",
+                        ImageUrl = "images/src",
+                        ReleaseDate = DateTime.Now,
+                        Owner = "Jay"
+                    }
+                 );
+
+            return await Task.FromResult(movieList);*/
+        }
+
+        public async Task<UserInfoViewModel> GetUserInfo()
+        {
+            var idpClient = _httpClientFactory.CreateClient("IDPClient");
+
+            var metaDataResponse = await idpClient.GetDiscoveryDocumentAsync();
+            if (metaDataResponse.IsError)
+            {
+                throw new HttpRequestException("Something went wrong while requesting the access token!");
+            }
+            var accessToken = await _httpContextAccessor
+                .HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+
+            var userInfoResponse = await idpClient.GetUserInfoAsync(
+                new UserInfoRequest
+                {
+                    Address = metaDataResponse.UserInfoEndpoint,
+                    Token = accessToken
+                });
+
+            if (userInfoResponse.IsError)
+            {
+                throw new HttpRequestException("Something went wrong while getting user info!");
+            }
+
+            var userInfoDictionary = new Dictionary<string, string>();
+
+            foreach(var claim in userInfoResponse.Claims)
+            {
+                userInfoDictionary.Add(claim.Type, claim.Value);
+            }
+
+            return new UserInfoViewModel(userInfoDictionary);
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
